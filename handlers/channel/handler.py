ea650dc284bed6f6f8c0668036e1e348ad86294e
Husanboyazamov <azamovhusanboy08@gmail.com>
2025-05-26 22:46:23 +0500

    New g4f version: 0.5.3.1 (current: 0.3.9.7) | pip install -U g4f

--- handlers/channel/handler.py
+++ handlers/channel/handler.py
@@ -1,7 +1,7 @@
-from utils.env import CHANNEL_ID
 from loader import bot
+from utils.env import CHANNEL_ID
 
-async def check_subscription(user_id):
+async def check_group_membership(user_id):
     try:
         member = await bot.get_chat_member(CHANNEL_ID, user_id)
         return member.status in ["member", "administrator", "creator"]
