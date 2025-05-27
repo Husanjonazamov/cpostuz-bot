0c790760a1321008e3c4424dcc0f9d70707788f9
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-05-27 09:06:40 +0000

    change

--- handlers/file_id.py
+++ handlers/file_id.py
@@ -1,19 +1,13 @@
-from aiogram.types import Message
-from aiogram.dispatcher import FSMContext
-
-from utils.env import ADMIN
-from loader import dp
-
-
-
-
-@dp.message_handler(content_types=['photo'])
-async def file_id(message: Message, state: FSMContext):
-    photo_id = message.photo[-1].file_id
-    if message.from_user.id == ADMIN:
-        await message.answer(photo_id)
+# from aiogram.types import Message
+# from aiogram.dispatcher import FSMContext
+# from utils.env import ADMIN
+# from loader import dp
 
 
 
 
-
+# @dp.message_handler(content_types=['photo'])
+# async def file_id(message: Message, state: FSMContext):
+#     if message.chat.type == "private" and message.from_user.id == ADMIN:
+#         photo_id = message.photo[-1].file_id
+#         await message.answer(photo_id)
