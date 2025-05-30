3774ee87ae2e6fec29d7a028e6625b55833755d3
Husanboyazamov <azamovhusanboy08@gmail.com>
2025-05-30 11:14:28 +0500

    Қисқа ва мазмунли қўшиш

--- handlers/file_id.py
+++ handlers/file_id.py
@@ -7,33 +7,28 @@ from utils import texts
 
 
 
-
 @dp.message_handler(content_types=['photo'])
 async def file_id(message: Message, state: FSMContext):
     photo_id = message.photo[-1].file_id
     user_id = message.from_user.id
-    if user_id == ADMIN:
-        await message.answer(
-            photo_id
-        )
-    else:
-        username = message.from_user.username
-        
-        user = getUser(user_id)
-        lang = user['data'][0]['lang']
-        phone = user['data'][0]['phone']
-        
-        caption = texts.caption_text(
-            username=username,
-            phone=phone
-        )
-        
-        await bot.send_photo(
-            chat_id=ADMIN,
-            photo=photo_id,
-            caption=caption
-        )
-        
-        await message.answer(
-            texts.SUCCESS_SCREEN[lang]
-        )
+    
+    username = message.from_user.username
+    
+    user = getUser(user_id)
+    lang = user['data'][0]['lang']
+    phone = user['data'][0]['phone']
+    
+    caption = texts.caption_text(
+        username=username,
+        phone=phone
+    )
+    
+    await bot.send_photo(
+        chat_id=ADMIN,
+        photo=photo_id,
+        caption=caption
+    )
+    
+    await message.answer(
+        texts.SUCCESS_SCREEN[lang]
+    )
